9fc4381e3da9270501961d42aeb42e6a593ea6f3
Xacone <benjamaayazid21@gmail.com>
2023-11-19 00:51:55 +0100

    + BestEdrOfTheMarket article

---
+++ js/imagezoom.js
@@ -0,0 +1,30 @@
+$('.without-caption').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+    image: {
+        verticalFit: true
+    },
+    zoom: {
+        enabled: true,
+        duration: 300 // don't foget to change the duration also in CSS
+    }
+});
+
+$('.with-caption').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    mainClass: 'mfp-with-zoom mfp-img-mobile',
+    image: {
+        verticalFit: true,
+        titleSrc: function(item) {
+            return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+        }
+    },
+    zoom: {
+        enabled: true
+    }
+});
+
